110af444d822fee495fcadd7a3ea65415aead9d6
Alex Grimm <alexander.grimm@ottogroup.com>
2023-03-12 15:26:29 +0100

    Add FFN layer correctly

--- python/modules.py
+++ python/modules.py
@@ -6,7 +6,7 @@ import torch
 
 class TransformerEncoder(nn.Module):
     def __init__(
-        self, d_model, vocab_size, num_encoder_blocks=2, multi_head_att_heads=2
+        self, d_model, vocab_size, num_encoder_blocks=2, multi_head_att_heads=2, ffn_hidden_dim=None,
     ):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, d_model)
@@ -25,13 +25,18 @@ class TransformerEncoder(nn.Module):
 
 
 class TransformerEncoderBlock(nn.Module):
-    def __init__(self, d_model, n_heads):
+    def __init__(self, d_model, n_heads, d_ffn=None):
         super().__init__()
         self.attn_dim = d_model // n_heads
 
         self.atn = MultiHeadAttentionLayer(d_model, n_heads)
         self.res_norm_1 = AddAndNorm(d_model)
-        self.lin = nn.Linear(d_model, d_model)
+
+        if d_ffn is None:
+            d_ffn = d_model
+        self.lin = nn.Sequential(
+            nn.Linear(d_model, d_ffn), nn.ReLU(), nn.Linear(d_ffn, d_model)
+        )
         self.res_norm_2 = AddAndNorm(d_model)
 
     def forward(self, X):
@@ -44,16 +49,18 @@ class TransformerEncoderBlock(nn.Module):
 
 if __name__ == "__main__":
     batch_size = 2
-    vocab_size = 4 # Number of words for embedding
-    max_time_length = 4 # Number of time steps or tokens per batch
-    d_model = 128 # Hidden dim size for all internal vectors, including embeddings
-    n_heads = 4 # Number of Attention heads in multihead attention blocks
+    vocab_size = 4  # Number of words for embedding
+    max_time_length = 4  # Number of time steps or tokens per batch
+    d_model = 128  # Hidden dim size for all internal vectors, including embeddings
+    ffn_hidden_dim = 1024
+    n_heads = 4  # Number of Attention heads in multihead attention blocks
 
     encoder = TransformerEncoder(
         d_model,
         vocab_size=vocab_size,
         num_encoder_blocks=2,
         multi_head_att_heads=n_heads,
+        ffn_hidden_dim=ffn_hidden_dim
     )
 
     # These are fake word ids
